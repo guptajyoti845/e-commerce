a9ecd6078ab906e9841445b3125bb35811eb9da3
guptajyoti845 <jyotiguptaofficial@gmail.com>
2021-03-13 16:40:00 +0530

    setup firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
+  // firebase: {
+  //   apiKey: 'AIzaSyAp-dFzcfAg2q3L7mF5HiYdYd4hxT0ndrI',
+  //   authDomain: 'e-commerce-7762f.firebaseapp.com',
+  //   projectId: 'e-commerce-7762f',
+  //   storageBucket: 'e-commerce-7762f.appspot.com',
+  //   messagingSenderId: '353073752558',
+  //   appId: '1:353073752558:web:3e4d6a914c8eb242d546ff',
+  //   measurementId: 'G-FSDSCTNNVV'
+  // }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false
+  // firebase: {
+  //   apiKey: 'AIzaSyAp-dFzcfAg2q3L7mF5HiYdYd4hxT0ndrI',
+  //   authDomain: 'e-commerce-7762f.firebaseapp.com',
+  //   projectId: 'e-commerce-7762f',
+  //   storageBucket: 'e-commerce-7762f.appspot.com',
+  //   messagingSenderId: '353073752558',
+  //   appId: '1:353073752558:web:3e4d6a914c8eb242d546ff',
+  //   measurementId: 'G-FSDSCTNNVV'
+  //
+  // }
 };
 
 /*
